f5e5e2d837306e6d48c86e78462f2dcfd07fecc0
eliardo <eliardosilva81@gmail.com>
2023-03-06 17:57:09 -0300

    app de convidados, que tem o intuito de cadastrar e ver lista convidados, feats ja realizadas projeto em mvvm, view binding, sqlite

---
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -0,0 +1,39 @@
+package com.example.convidados.repository
+
+import android.content.ContentValues
+import android.content.Context
+import com.example.convidados.constants.DataBaseConstants
+import com.example.convidados.model.GuestModel
+
+class GuestRepository private constructor(context: Context) {
+    private val guestDataBase = GuestDataBase(context)
+
+
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context): GuestRepository {
+            if (!Companion::repository.isInitialized) {
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
+
+    fun insert(guest: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val presence = if (guest.present) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.presence, presence)
+
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, values)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
